53e748b076cff300cc5526e88c90f33126c8955f
don peter <donpeter212@gmail.com>
2022-03-02 15:12:42 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Home from './component/homepage/Home';
+import Login from "./component/login/Login"
+import Register from "./component/register/Register"
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import { useState } from 'react';
 
 function App() {
+
+  const [ user, setLoginUser] = useState({})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/">
+            {
+              user && user._id ? <Home setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser}/>
+            }
+          </Route>
+          <Route path="./component/login/Login" element={<Home/>}>
+            <Login setLoginUser={setLoginUser}/>
+          </Route>
+          <Route path="./component/register/Register" element={<Register/>}>
+          </Route>
+          <Register />
+          </Routes>
+      </Router>
     </div>
   );
 }
